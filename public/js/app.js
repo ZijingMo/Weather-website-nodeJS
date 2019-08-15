2e17a67bb5a8e477a10414c299415c7442dbecbe
Zijing Mo <ZijingMo@mtechs-MacBook-Pro.local>
2019-08-14 19:34:11 -0500

    hide console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Clinet side javascript file is loaded!')
+//console.log('Clinet side javascript file is loaded!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
