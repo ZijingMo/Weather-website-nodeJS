7fee70e83869696fe0d7a3ae725d379bcfe48f46
Zijing Mo <ZijingMo@mtechs-MacBook-Pro.local>
2019-08-19 09:58:13 -0500

    /public/js/app.js Updated

--- public/js/app.js
+++ public/js/app.js
@@ -5,16 +5,24 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-//messageOne.textContent = 'From JS'
-//messageTwo.textContent = 'Hi'
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const address = search.value
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch('http://localhost:3000/weather?address=' + address).then((res) => {
-            res.json().then((data) => {
+    fetch('http://localhost:3000/weather?address=' + address)
+    .then((res) => {
+            res.json()
+            .then((data) => {
+                console.log(data.location)
+                console.log(data.forecast)
+                messageOne.textContent = `${data.location}`
+                messageTwo.textContent = `${data.forecast}`
+            })
+            .catch(err => console.log(err))   
+        /*
+        // This segment of code need to be modified in the future
+        {
             if (data.error) {
                 console.log('Something wrong for data fetch:' + data.error)
                 messageTwo.textContent = data.error
@@ -24,8 +32,14 @@ weatherForm.addEventListener('submit', (e) => {
                 messageOne.textContent = `${data.location}`
                 messageTwo.textContent = `${data.forecast}`
             }
-        })
-      })  
+        }
+        */      
+      }) 
+      .catch(err => {
+          console.log(err)
+          messageOne.textContent = 'Opps! The address you inputed is incorrect. Please restart the application from the server side.'
+          messageTwo.textContent = `${err}`
+    }) 
     //console.log(address)
 })
 
