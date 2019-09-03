aeb27de3c08c127b0fb9a2a9f1951425ac4bdd43
Zijing Mo <ZijingMo@mtechs-MBP.hsd1.mn.comcast.net>
2019-09-02 22:17:51 -0500

    new features added (forecase.js change)

--- src/utils/forecase.js
+++ src/utils/forecase.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find the location..', undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${(body.currently.precipIntensity).toFixed(2) * 100}% chance of rain.`)
+            callback(undefined, `Current weather conditions: ${body.currently.summary}, ${body.currently.temperature} degrees out, ${(body.currently.precipProbability).toFixed(2) * 100}% chance of rain, ${(body.currently.humidity).toFixed(2) * 100}% outdoor humidity, ${(body.currently.visibility).toFixed(2)} miles visibility. Tomorrow weather conditions: ${body.daily.data[1].summary} ${body.daily.data[1].temperatureHigh} degrees high, ${body.daily.data[1].temperatureLow} degrees low, ${body.daily.data[1].uvIndex} UV Index.`)
         }
     })
 }
